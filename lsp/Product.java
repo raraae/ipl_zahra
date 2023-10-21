2b3011b4ab5e8fa7fde5a80d914dd8b50c2b70ff
AzzahraAsriRomadhoni <asriromadhoniazzahra@gmail.com>
2023-10-21 21:41:23 +0700

    Commit LSP

---
+++ lsp/Product.java
@@ -0,0 +1,11 @@
+abstract class Product {
+    abstract String getName();
+
+    /**
+     * * Function to get all of information about product
+     */
+
+    public void getProductInfo() {
+        // some magic code
+    }
+}
---
+++ lsp/Smartphone1.java
@@ -0,0 +1,6 @@
+class Smartphone1 extends Product {
+    @Override
+    String getName() {
+        return "Samsung S10+ Limited Edition";
+    }
+}
